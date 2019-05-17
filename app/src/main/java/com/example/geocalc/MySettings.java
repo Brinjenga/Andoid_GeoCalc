a3e9df4c01775e1682704337534abbba3cabf3e1
brinjenga <briannjenga413@gmail.com>
2019-05-17 15:14:48 -0400

    setting units options

--- app/src/main/java/com/example/geocalc/MySettings.java
+++ app/src/main/java/com/example/geocalc/MySettings.java
@@ -26,7 +26,8 @@ public class MySettings extends AppCompatActivity {
         FloatingActionButton fab =  findViewById(R.id.fab);
         fab.setOnClickListener(view -> {
             Intent intent = new Intent();
-            intent.putExtra("bOptions",optionKm);
+            intent.putExtra("dOptions",optionKm);
+            intent.putExtra("bOptions",optionDegrees);
             setResult(MainActivity.Dunits);
             finish();
         });
@@ -35,15 +36,34 @@ public class MySettings extends AppCompatActivity {
         distanceUnits = findViewById(R.id.distanceUnits);  //creating a spinner variable
         bearingUnits = findViewById(R.id.bearingUnits);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource( this,
-             R.array.bOptions, android.R.layout.simple_spinner_dropdown_item);
+        //distance adapter
+        ArrayAdapter<CharSequence> dadapter = ArrayAdapter.createFromResource( this,
+             R.array.dOptions, android.R.layout.simple_spinner_dropdown_item);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        distanceUnits.setAdapter(adapter);
+        dadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        distanceUnits.setAdapter(dadapter);
         distanceUnits.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long id) {
                 optionKm = (String) adapterView.getItemAtPosition(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+          //bearing adapter
+        ArrayAdapter<CharSequence> badapter = ArrayAdapter.createFromResource( this,
+                R.array.bOptions, android.R.layout.simple_spinner_dropdown_item);
+
+        badapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        bearingUnits.setAdapter(badapter);
+        bearingUnits.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long id) {
+                optionDegrees = (String) adapterView.getItemAtPosition(i);
             }
 
             @Override
